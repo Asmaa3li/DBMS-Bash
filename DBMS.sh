d3b7522969d8f5afe097b113f1540b36866c75f7
Asmaa <asmaa@192.168.1.10>
2024-03-08 02:25:03 +0200

    add style

--- DBMS.sh
+++ DBMS.sh
@@ -5,6 +5,28 @@ mkdir -p ./Database 2>> ./logs.txt
 # cd ./Database
 echo -e "\n\e[33m**************************Welcome To Our DBMS**************************\e[0m\n" 
 #source ./mainFunctions.sh
+red="\033[0;31m"
+green="\033[0;32m"
+yellow="\033[0;33m"
+blue="\033[0;34m"
+reset="\033[0m" 
+
+
+bar=""
+for ((i=0; i<50; i++)); do
+    bar+="\u2500"
+done
+
+# Print 
+echo -e "$bar\u252c$bar\u2500\u2500"
+echo -e "${red}                                          Welcome${green} To${yellow} Our${blue} DBMS${reset}"
+echo -e "$bar\u2534$bar\u2500\u2500"
+
+
+
+
+
+
 
 parentMenu
 
